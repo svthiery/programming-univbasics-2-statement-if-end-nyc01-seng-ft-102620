d90f5caeb5a67df206392ff91826fa9bd49ac036
Sarah Kain <sarahvkain@gmail.com>
2020-10-04 22:11:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++
@@ -1,8 +0,0 @@
-chance_of_rain = 0.3 
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-else 
-  puts "Stay home and read Hegel."
-end
