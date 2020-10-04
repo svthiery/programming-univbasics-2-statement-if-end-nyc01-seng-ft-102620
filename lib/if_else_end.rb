3f46e556cac89e37defdca3bbc808f419ea916a8
Sarah Kain <sarahvkain@gmail.com>
2020-10-04 22:05:33 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,8 @@
-run_code_inside = false  
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
+chance of rain = 0.2 
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else 
+  puts "Stay home and read Hegel."
 end
-puts "Code after if...end"
